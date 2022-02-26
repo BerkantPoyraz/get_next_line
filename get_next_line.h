0dd71e1784d313d419cdbfc5d14cbb04b8d61ee1
790 <91029089+Nulda@users.noreply.github.com>
2022-02-26 22:42:13 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef   GET_NEXT_LINE_H
+# define  GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t   ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_strchr(const char *str, int c);
+char    *ft_line(char *src);
+char    *ft_line_save(char *src);
+char    *ft_first_line(char *src, int fd);
+char    *get_next_line(int fd);
+
+#endif
